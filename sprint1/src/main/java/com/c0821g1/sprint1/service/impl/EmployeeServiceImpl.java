3f63f52f8cd0e9eea35a70868dcc8fe8ea3c62c9
NguyenThanhDuong113bn <nguyenthanhduong113bn@gmail.com>
2022-03-02 17:30:14 +0700

    update

---
+++ sprint1/src/main/java/com/c0821g1/sprint1/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,25 @@
+package com.c0821g1.sprint1.service.impl;
+
+import com.c0821g1.sprint1.entity.employee.Employee;
+import com.c0821g1.sprint1.repository.EmployeeRepository;
+import com.c0821g1.sprint1.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Override
+    public Employee findEmployeeByAppUser(String username) {
+        return employeeRepository.findEmployeeByAppUser(username);
+    }
+
+    @Override
+    public Page<Employee> findAllEmployeePage(Pageable pageable) {
+        return null;
+    }
+}
