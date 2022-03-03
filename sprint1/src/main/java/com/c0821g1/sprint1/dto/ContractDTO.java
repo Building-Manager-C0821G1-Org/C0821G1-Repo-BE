abeeb6df5806d2d408cef696582c09cddd5ba0d8
tayngo97 <anhtayd27111997@gmail.com>
2022-03-03 14:57:10 +0700

    update 2:56pm 03-03

--- sprint1/src/main/java/com/c0821g1/sprint1/dto/ContractDTO.java
+++ sprint1/src/main/java/com/c0821g1/sprint1/dto/ContractDTO.java
@@ -1,9 +1,11 @@
 package com.c0821g1.sprint1.dto;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public class ContractDTO implements Validator {
 
@@ -19,14 +21,18 @@ public class ContractDTO implements Validator {
 
     private int contractId;
 
+    @NotNull
     @NotBlank
     private String contractExpred;
 
+    @NotNull
     @NotBlank
     private String contractDateStart;
 
+    @NotNull
     @NotBlank
     private String contractDateEnd;
+
 
     @NotBlank
     private String price;
@@ -35,7 +41,9 @@ public class ContractDTO implements Validator {
     private String contractTotal;
 
     @NotBlank
+    @Length(min = 5)
     private String contractContent;
+
     private Integer employeeId;
     private Integer customerId;
     private Integer spaceId;
@@ -44,7 +52,7 @@ public class ContractDTO implements Validator {
     public ContractDTO() {
     }
 
-    public ContractDTO(int contractId, @NotBlank String contractExpred, String contractDateStart, String contractDateEnd, String price, String contractTotal, String contractContent, Integer employeeId, Integer customerId, Integer spaceId, Boolean contractDeleteFlag) {
+    public ContractDTO(int contractId, String contractExpred, String contractDateStart, String contractDateEnd, String price, String contractTotal, String contractContent, Integer employeeId, Integer customerId, Integer spaceId, Boolean contractDeleteFlag) {
         this.contractId = contractId;
         this.contractExpred = contractExpred;
         this.contractDateStart = contractDateStart;
