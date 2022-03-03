3a50db4dd36091ff9d2b7572b1846ac5e7f72083
NguyenThanhDuong113bn <nguyenthanhduong113bn@gmail.com>
2022-03-03 16:38:58 +0700

    update

--- C0821G1-spring-boot-junit-5/c08g-spring-boot-junit-5/src/main/java/com/codegym/repository/IClassStudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.repository;
-
-import com.codegym.model.ClassStudent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IClassStudentRepository extends JpaRepository<ClassStudent, Integer> {
-}
--- C0821G1-spring-boot-junit-5/c08g-spring-boot-junit-5/src/main/java/com/codegym/service/IClassStudentService.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.service;
-
-import com.codegym.model.ClassStudent;
-
-import java.util.List;
-
-public interface IClassStudentService {
-    List<ClassStudent> findAll();
-}
