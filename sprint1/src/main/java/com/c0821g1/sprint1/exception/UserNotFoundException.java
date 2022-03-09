e18675321a94e5254618e9533e38e8089df87c54
tayngo97 <anhtayd27111997@gmail.com>
2022-03-09 09:21:55 +0700

    update

--- sprint1/src/main/java/com/c0821g1/sprint1/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.c0821g1.sprint1.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
--- sprint1/src/main/java/com/c0821g1/sprint1/service/SpaceService.java
+++
@@ -1,5 +0,0 @@
-package com.c0821g1.sprint1.service;
-
-
-public interface SpaceService {
-}
