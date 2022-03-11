981ddadc115c9252d92760b1f3c1aa1dd9662e85
Duy Nguyen <you@example.com>
2022-03-11 16:38:48 +0700

    up

--- sprint1/src/main/java/com/c0821g1/sprint1/repository/FloorsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.c0821g1.sprint1.repository;
-
-import com.c0821g1.sprint1.entity.floor.Floors;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FloorsRepository extends JpaRepository<Floors,Integer> {
-}
--- sprint1/src/main/java/com/c0821g1/sprint1/service/impl/SpaceServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.c0821g1.sprint1.service.impl;
-
-import com.c0821g1.sprint1.service.SpaceService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class SpaceServiceImpl implements SpaceService {
-}
