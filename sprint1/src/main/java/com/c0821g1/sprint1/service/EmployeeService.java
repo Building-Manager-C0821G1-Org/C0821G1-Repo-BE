999057d20ee9e2850ae67a731adfae383a18ec5c
Duy Nguyen <you@example.com>
2022-03-11 16:38:08 +0700

    up

--- sprint1/src/main/java/com/c0821g1/sprint1/service/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.c0821g1.sprint1.service;
-
-
-import com.c0821g1.sprint1.entity.employee.Employee;
-
-public interface EmployeeService {
-    Employee getEmployeeByUsername(String username);
-}
