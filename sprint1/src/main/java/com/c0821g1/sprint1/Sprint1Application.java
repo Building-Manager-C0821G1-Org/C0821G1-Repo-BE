b1c7d9b9589a96add55d278d8b30d187082566f3
nghiadoan123 <minhnghia191189@gmail.com>
2022-03-12 11:07:00 +0700

    nghia update

---
+++ sprint1/src/main/java/com/c0821g1/sprint1/Sprint1Application.java
@@ -0,0 +1,27 @@
+package com.c0821g1.sprint1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class Sprint1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sprint1Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/employee/**").allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
+
+}
